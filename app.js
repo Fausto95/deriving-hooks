f50732619e145205dc39067fad9be8617248afa0
Kyle Simpson <getify@gmail.com>
2020-07-23 09:11:05 -0500

    initial stuff

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function app(){
+	var root = document.getElementById("root");
+	var div = document.createElement("div");
+	root.appendChild(div);
+	div.innerHTML = "Hello world!";
+})();
