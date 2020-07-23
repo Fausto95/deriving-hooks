3efa743b7c0722e4c6b015a35b1622c44f0df2e9
Kyle Simpson <getify@gmail.com>
2020-07-23 09:19:07 -0500

    checkpoint 1: adding countdownTimer 'component' function

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 "use strict";
 
+function countdownTimer(counter) {
+	return `
+		<span>${
+			(counter > 0) ?
+				`Counting down: ${ counter }` :
+				"Finished!"
+		}
+		</span>
+	`;
+}
+
+
+
+
+// *************************
+
 (function app(){
 	var root = document.getElementById("root");
 	var div = document.createElement("div");
 	root.appendChild(div);
-	div.innerHTML = "Hello world!";
+	div.innerHTML = countdownTimer(5);
 })();
